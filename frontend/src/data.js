5a2975d49a68b7dcd05ecaefff71de25b5817cb0
unknown <khourynour79@gmail.com>
2022-11-17 09:18:56 +0200

    first

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+        _id: '1',
+        name: 'Bershka Slim Dress',
+        category: 'Shirts',
+        image: '/images/product-1.jpg',
+        price: 100 ,
+        brand:'Bershka',
+        rating: 4.0 ,
+        numReviews: 10 ,
+        description: 'high quality product'
+        },
+        {
+        _id: '2',
+        name: 'Zara blazer',
+        category: 'Suits',
+        image: '/images/product-2.jpg',
+        price: 120 ,
+        brand:'Zara',
+        rating: 4.5 ,
+        numReviews: 10 ,
+        description: 'high quality product'
+        },
+        {
+        _id: '3',
+        name: 'Lacoste free Shirt',
+        category: 'Shirts',
+        image: '/images/product-3.jpg',
+        price: 220 ,
+        brand:'Lacoste',
+        rating: 4.8 ,
+        numReviews: 17 ,
+        description: 'high quality product'
+        },
+        {
+        _id: '4',
+        name: 'Bershka Slim Pants',
+        category: 'Pants',
+        image: '/images/product-4.jpg',
+        price: 78 ,
+        brand:'Bershka',
+        rating: 4.5 ,
+        numReviews: 14 ,
+        description: 'high quality product'
+        },
+        {
+        _id: '5',
+        name: 'Puma Slim Pants',
+        category: 'Pants',
+        image: '/images/product-5.jpg',
+        price: 65 ,
+        brand:'Puma',
+        rating: 4.5 ,
+        numReviews: 10 ,
+        description: 'high quality product'
+        },
+        {
+        _id: '6',
+        name: 'Stradicarius Fit Pants',
+        category: 'Pants',
+        image: '/images/product-6.jpg',
+        price: 139 ,
+        brand:'Addidas',
+        rating: 4.5 ,
+        numReviews: 15 ,
+        description: 'high quality product'
+        }
+    ],
+}
+
+export default data;
